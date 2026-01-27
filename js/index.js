c738cb3de2ebd64136490e50f82c4bf8d0ade51f
Naiem00 <tsnayeem@gmail.com>
2026-01-27 14:10:28 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener("DOMContentLoaded", function () {
+    const items = document.querySelectorAll(".item");
+    items.forEach(function (element, index) {
+        setTimeout(function () {
+            element.classList.add("fade-in");
+        }, 200 * index);
+    });
+});
